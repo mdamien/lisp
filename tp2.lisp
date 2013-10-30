274b4f973bf7f8892817fff0bdb0c3824107b769
Damien <damien@dam.io>
2013-10-30 17:16:21 +0100

    it works dude! It works!

--- tp2.lisp
+++ tp2.lisp
@@ -1,4 +1,4 @@
-;state = ( 'nb de missionaires sur la rive gauche' 'nb de cannibales sur la rive gauche' 'coté de la rive où il y a le bateau')
+;state = ( 'nb de missionaires du mau' 'nb de cannibales sur la rive gauche' 'nb de bateau sur la rive gauche')
 (setq R-ops '((0 1) (1 0) (1 1) (2 0) (0 2)) )       ; <-
 (setq L-ops '((0 -1) (-1 0) (-1 -1) (-2 0) (0 -2)) ) ; ->
 
@@ -8,23 +8,35 @@
 )
 
 (defun apply-op (state op)
-	(list (+ (car state) (car op)) (+ (cadr state) (cadr op)) (if (eq (caddr state) 'R) 'L 'R))
+	(list (+ (car state) (car op)) (+ (cadr state) (cadr op)) (if (eq (caddr state) 1) 0 1))
 )
 
 ;parcours en profondeur
 (defun solve (state previous-states)
+	;(print state)
+	;(print previous-states)
 	(cond
-		((equal state '(0 0 R))
+		((equal state '(0 0 0))
 			(print previous-states))
+		;invalide les états invalides
 		((or (< (car state) 0) (< (cadr state) 0) nil))
 		((or (> (car state) 3) (> (cadr state) 3) nil))
-		;TODO: verifier nb de sauvages supérieurs aux missionaires
-		( T (dolist (op (if (eq (caddr state) 'R) R-ops L-ops))
-			(if (eq (list-contains (apply-op state op) previous-states) 'F) ; TODO: verifier "pendant qu'ils prennent le bateau"
+		;TODO: verifier nb de sauvages supérieurs aux missionaires sur l'autre rive
+		(
+			(or (and (eq (caddr state) 1) (> (cadr state) (car state)))
+			(and (eq (caddr state) 0) (< (cadr state) (car state)))
+			)
+			nil
+		)
+		;appliquer toutes les transformations possibles
+		( T (dolist (op (if (eq (caddr state) 0) R-ops L-ops))
+			;verifier que l'on ne retourne pas dans un état déjà 
+			(if (eq (list-contains (apply-op state op) previous-states) 'F)
 				(solve (apply-op state op) (cons (apply-op state op) previous-states))
 			)
 		))
 	)
 )
-(print (list-contains '(1 3) '( (1 2) (3 4))))
-(print (solve '(3 3 L) '()))
+
+;TODO:parcours en largeur
+(print (solve '(3 3 1) '()))
